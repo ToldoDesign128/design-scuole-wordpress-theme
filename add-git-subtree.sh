fd07c69a8dff04212098140c379d333ff455956b
Max Morozov <max@morozov.page>
2022-07-18 09:55:25 +0200

    Add script to create git subtree

---
+++ add-git-subtree.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# script to add git subtree to the project
+
+git remote add -f design-scuole https://github.com/maxmorozoff/design-scuole-pagine-statiche.git
+git subtree add --prefix assets/design-scuole design-scuole tag-dev --squash
+git fetch design-scuole tag-dev
+git subtree pull --prefix assets/design-scuole design-scuole tag-dev --squash
