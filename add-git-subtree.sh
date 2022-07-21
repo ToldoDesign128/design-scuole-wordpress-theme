a24292ef978d28d07f8eb3fc44fd4aa883318c87
Max Morozov <max@morozov.page>
2022-07-21 09:44:33 +0200

    Add script for git subtree

--- add-git-subtree.sh
+++ add-git-subtree.sh
@@ -6,3 +6,15 @@ git remote add -f design-scuole https://github.com/maxmorozoff/design-scuole-pag
 git subtree add --prefix assets/design-scuole design-scuole tag-dev --squash
 git fetch design-scuole tag-dev
 git subtree pull --prefix assets/design-scuole design-scuole tag-dev --squash 
+
+cd assets/design-scuole
+
+# install node modules
+npm install
+
+# install sass compiler
+sudo npm install sass -g
+
+# start file watch
+# sass --watch <input-file-path> <output-file-path>
+sass --watch src/scss/scuole.scss src/scss/scuole.css 
