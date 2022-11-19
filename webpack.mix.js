c24c406bbe380dd6de4f14d0630599fab80dfa11
Max Morozov <max@morozov.page>
2022-11-19 04:01:08 +0100

    Add scss admin styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -29,10 +29,14 @@ mix
     .sass('assets/css/martino.scss', 'assets/css')
     .sass('assets/css/martino-carousel.scss', 'assets/css')
     .options({
+        processCssUrls: false,
         watchOptions: {
             ignored: /node_modules/
         }
     });
+mix
+    .sass('assets/css/martino-admin.scss', 'inc/admin-css/style-admin.css')
+    .options({ processCssUrls: false });
 
 if (process.env.MIX_NOTIFICATIONS == 'false') {
     mix.disableNotifications();
